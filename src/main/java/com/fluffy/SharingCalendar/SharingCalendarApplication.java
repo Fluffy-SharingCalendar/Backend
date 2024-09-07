afd2c0ab60bdaeebabc76dfcaf2337c1434966d7
yujeongkwon <87534067+yujeongkwon@users.noreply.github.com>
2024-09-07 21:43:42 +0900

    ☁ init : first commit

---
+++ src/main/java/com/fluffy/SharingCalendar/SharingCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.fluffy.SharingCalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharingCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharingCalendarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fluffy/SharingCalendar/SharingCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fluffy.SharingCalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharingCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
