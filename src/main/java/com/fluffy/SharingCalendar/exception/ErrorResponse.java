8cd55e2a4d403aa39c1032c994a80df83c406674
yujeongkwon <87534067+yujeongkwon@users.noreply.github.com>
2024-09-26 01:20:31 +0900

    fix() : record 변환 후 어노테이션 제거

--- src/main/java/com/fluffy/SharingCalendar/exception/ErrorResponse.java
+++ src/main/java/com/fluffy/SharingCalendar/exception/ErrorResponse.java
@@ -1,12 +1,5 @@
 package com.fluffy.SharingCalendar.exception;
 
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-
-@Getter
-@EqualsAndHashCode
-@AllArgsConstructor
 public record ErrorResponse(Integer errorCode, String message) {
     public static ErrorResponse toErrorResponse(ErrorCode errorCode) {
         return new ErrorResponse(errorCode.getHttpStatus().value(), errorCode.getMessage());
